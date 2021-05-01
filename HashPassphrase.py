dee461a564e2adb806e141079d9d3d2a700089da
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-01 21:21:36 +0200

    fix bug

--- HashPassphrase.py
+++ HashPassphrase.py
@@ -4,25 +4,32 @@ from hashlib import pbkdf2_hmac
 import string
 
 chars = string.digits + string.ascii_letters + string.punctuation
-salt = 'pepper'
+salt = "pepper"
 
-def convert_bytes_to_password(hashed_bytes, length):
+
+def convert_bytes_to_string(hashed_bytes, length):
     number = int.from_bytes(hashed_bytes, byteorder="big")
-    password = ""
-    while number > 0 and len(password) <length:
-        password = password + chars[number % len(chars)]
+    string = ""
+    while number > 0 and len(string) < length:
+        string = string + chars[number % len(chars)]
         number = number // len(chars)
-    return password
+    return string
 
-def convert_hash_string_to_password(hash_string):
-    hashed_bytes = pbkdf2_hmac("sha512", hash_string.encode("utf-8"), salt.encode("utf-8"), 4096)
-    password = convert_bytes_to_password(hashed_bytes, 16)
-    return password
 
-if __name__ == '__main__':
-    hash_string = input('Hash String >>>')
+def convert_hash_string(hash_string):
+    hashed_bytes = pbkdf2_hmac(
+        "sha512", hash_string.encode("utf-8"), salt.encode("utf-8"), 4096
+    )
+    hashed_string = convert_bytes_to_string(hashed_bytes, 16)
+    return hashed_string
+
+
+if __name__ == "__main__":
+    hash_string = input("Hash String >>>")
 
     while len(hash_string) < 1:
-        hash_string = input('Hash String >>>')
+        hash_string = input("Hash String >>>")
 
-    print(F'Hashed Passphrase >>> {hash_string}')
+    hashed_string = convert_hash_string(hash_string)
+
+    print(f"Hashed Passphrase >>> {hashed_string}")
